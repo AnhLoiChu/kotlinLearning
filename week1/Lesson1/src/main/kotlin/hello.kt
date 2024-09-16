8ae34b808bdc648deac2a698ad7fc8f74178e46c
AnhLoiChu <146374016+AnhLoiChu@users.noreply.github.com>
2024-09-16 12:54:41 +0700

    Add files via upload

---
+++ week1/Lesson1/src/main/kotlin/hello.kt
@@ -0,0 +1,17 @@
+
+fun main(){
+    printHello()
+    forloop()
+    repeatloop()
+}
+fun printHello(){
+    println(" Hello World")
+}
+fun forloop(){
+    for (i in 1..5)
+        print(i)
+}
+fun repeatloop(){
+    repeat(2){
+    println("Its good")}
+}

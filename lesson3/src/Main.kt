0bb39b1c27cb6d8f4efd0990faa2966d9d3eabaf
AnhLoiChu <146374016+AnhLoiChu@users.noreply.github.com>
2024-09-29 21:43:45 +0700

    Add files via upload

---
+++ lesson3/src/Main.kt
@@ -0,0 +1,11 @@
+fun main(){
+    open class AquariumPlant(val color: String, private val size: Int)
+class GreenLeafyPlant(size: Int) : AquariumPlant("green", size)
+fun AquariumPlant.print() = println("AquariumPlant")
+fun GreenLeafyPlant.print() = println("GreenLeafyPlant")
+val plant = GreenLeafyPlant(size = 10)
+plant.print()
+println("\n")
+val aquariumPlant: AquariumPlant = plant
+aquariumPlant.print()  // what will it print?
+}
